6d044eb7d5d41ce0ceac0768b9a74a9e0747e660
sanusftgit <sanujan5238@gmail.com>
2025-07-24 23:02:38 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
- window.addEventListener('load', () => {
-    document.getElementById('preloader').style.display = 'none';
-    AOS.init();
-  });
-  var typed = new Typed('.typed-text', {
-    strings: ["Web-Developer", "UI/UX Designer", "Tech-Student"],
-    typeSpeed: 80,
-    backSpeed: 50,
-    loop: true
+  window.addEventListener("load", () => {
+    const preloader = document.getElementById("preloader");
+    if (preloader) {
+      preloader.style.opacity = "0";
+      setTimeout(() => {
+        preloader.style.display = "none";
+      }, 600);
+    }
   });
